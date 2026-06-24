0260abb043f340cc2fb6d05825c5051bb7cf7ca7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-24 15:55:48 +0000

    [fern-generated] Update SDK
    Generated by Fern
    CLI Version: unknown
    Generators:
      - fernapi/fern-typescript-sdk: 3.72.10

--- src/api/errors/RequestTimeoutError.ts
+++ src/api/errors/RequestTimeoutError.ts
@@ -17,6 +17,6 @@ export class RequestTimeoutError extends errors.NewscatcherApiError {
             Error.captureStackTrace(this, this.constructor);
         }
 
-        this.name = this.constructor.name;
+        this.name = "RequestTimeoutError";
     }
 }
--- src/errors/NewscatcherApiTimeoutError.ts
+++ src/errors/NewscatcherApiTimeoutError.ts
@@ -10,7 +10,7 @@ export class NewscatcherApiTimeoutError extends Error {
             Error.captureStackTrace(this, this.constructor);
         }
 
-        this.name = this.constructor.name;
+        this.name = "NewscatcherApiTimeoutError";
         if (opts?.cause != null) {
             this.cause = opts.cause;
         }
